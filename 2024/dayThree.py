ab3929197994025efbf2048f28943d4d8c5c7d89
A <alyssa@kvasl.com>
2024-12-03 20:29:24 +1300

    Created mul function

--- 2024/dayThree.py
+++ 2024/dayThree.py
@@ -24,7 +24,16 @@
 
 import re
 
+#Multiply two numbers function
+def mul(x,y):
+    return x * y
+
+
 corruptString = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
 
-mul = re.findall('mul[(][0-9]{1,3},[0-9]{1,3}[)]', corruptString) #Finds all of the strings using regEx
-print(mul)
+validMul = re.findall('mul[(][0-9]{1,3},[0-9]{1,3}[)]', corruptString) #Finds all of the strings using regEx
+print(validMul)
+
+for strings in validMul:
+     pass
+
