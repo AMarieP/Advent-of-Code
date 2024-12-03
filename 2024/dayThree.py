707a2d4da9e0341e001b594e8f38af1c6affad9e
A <alyssa@kvasl.com>
2024-12-03 21:23:07 +1300

    Updated regex done

--- 2024/dayThree.py
+++ 2024/dayThree.py
@@ -60,3 +60,10 @@ stringUncorrupter(corruptString)
 
 # Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
 
+newCorruptString = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+validMul = re.findall("mul[(][0-9]{1,3},[0-9]{1,3}[)]|do[(][)]|don't[(][)]", newCorruptString)
+
+
+
+print(validMul)
